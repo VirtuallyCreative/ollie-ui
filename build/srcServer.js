120fd81529bcfea73f0d345d58fc96e8565c78bb
Vip3rousmango <alexander@virtuallycreative.ca>
2018-10-22 13:11:36 -0400

    Added NPM Scripts & Started Babel file

--- build/srcServer.js
+++ build/srcServer.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+var express = require('express')
+var path = require('path')
+var open = require('open')
 
-var port = 3000;
-var app = express();
+var port = 3000
+var app = express()
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../src/index.html'));
-});
+    res.sendFile(path.join(__dirname, '../src/index.html'))
+})
 
 app.listen(port, function(err) {
     if (err) {
-        console.log(err);
+        console.log(err)
 
     } else {
-        open('http://localhost:' + port);
+        open('http://localhost:' + port)
     }
-});
+})
