8f9545a4efa803b59379d8e89c08dbf0068a694d
Vip3rousmango <alexander@virtuallycreative.ca>
2018-10-25 16:27:42 -0400

    Tweaked test with JSDOM scaffold

--- src/index.test.js
+++ src/index.test.js
@@ -3,11 +3,20 @@ import jsdom from 'jsdom';
 const { JSDOM } = jsdom;
 
 describe('index.html', () => {
+  //Scaffold dom to regular document object
+  before(function() {
+    return JSDOM.fromFile('src/index.html')
+      .then((dom) => {
+        global.window = dom.window;
+        global.document = window.document;
+      });
+    })
+  // Start 'it should' assertion like normal...
   it('should include Ollie\'s Hello World', (done) => {
-      JSDOM.fromFile("index.html").then(dom => {
-        const h1 = dom.window.document.getElementsByClassName('title')
-        expect(h1.innerHTML).to.equal("Welcome to Ollie Framework!");
-      })
-      done();
+        expect(document.getElementsByTagName('h1')[0].innerHTML).to.equal("Welcome to Ollie Framework!");
+        done();
+        window.close();
     })
 })
+
+
