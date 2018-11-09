0002e0ee5870b52d1d199153838bd8d305a4a1ea
Vip3rousmango <alexander@virtuallycreative.ca>
2018-11-09 14:04:14 -0500

    Added Production Build process

---
+++ build/build.js
@@ -0,0 +1,33 @@
+/*eslint-disable no-console */
+import webpack from 'webpack'
+import webpackConfig from '../webpack.config.prod'
+import chalk from 'chalk'
+
+process.env.NODE_ENV = 'production'
+
+// Let's Dev know *something* is occuring...
+console.log(chalk.blue('Minifying bundle for production. This will take a sec...'))
+
+const jsonStats = stats.toJson();
+
+if (jsonStats.hasErrors) {
+  return jsonStats.errors.map(error => console.log(chalk.red(error)))
+}
+
+if (jsonStats.hasWarnings) {
+  console.log(chalk.yellow('Webpack generated the following warnings: '))
+  jsonStats.warnings.map(warning => console.log(chalk.yellow(warning)))
+}
+
+console.log(`Webpack stats: ${stats}`)
+
+// if we got this far, then build is successful!
+console.log(chalk.green('Your app has been built for production and written to /dist!'))
+
+webpack(webpackConfig).run((err, stats) => {
+  if (err) { // so, a fatal error just happened. Full stop.
+    console.log(chalk.red(err))
+    return 1
+  }
+  return 0
+})
---
+++ webpack.config.prod.js
@@ -0,0 +1,43 @@
+import webpack from 'webpack';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  mode: 'development',
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json']
+  },
+  devtool: 'source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: true,
+      noInfo: true // set to false to see a list of every file being bundled.
+    }),
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    }),
+    // Eliminate dupe packages
+    new webpack.optimize.DedupePlugin(),
+
+    // Minify JS
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+      {test: /\.css$/, use: ['style-loader','css-loader']}
+    ]
+  }
+}
