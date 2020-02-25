44eeadcb12e7830543388972534e46e34243158a
Vip3rousmango <alexander@virtuallycreative.ca>
2020-02-25 14:09:26 -0500

    fixed test docs for Mocha Sidebard Extension (VSCode)

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,33 @@
-import {expect} from 'chai';
-import jsdom from 'jsdom';
-import fs from 'fs';
+// Require Chai
+let { expect } = require('chai'),
+    jsdom = require('jsdom'),
+    path = require('path'),
+    fs = require('fs');
 
-describe('Our first test', () => {
-  it('should pass', () => {
-    expect(true).to.equal(true);
-  });
-});
+
+// describe('Our first test', () => {
+//   it('should pass', () => {
+//     expect(true).to.equal(true);
+//   });
+// });
 
 describe('index.html', () => {
-  it('should have h1 that says Users', () => {
-    const index = fs.readFileSync('./src/index.ejs', "utf-8");
-    const { JSDOM } = jsdom;
-    const dom = new JSDOM(index);
-    const h1 = dom.window.document.getElementsByTagName("h1")[0];
-    expect(h1.innerHTML).to.equal("Users");
-    dom.window.close();
-  })
+    it('should have h1 that says Users', () => {
+        const index = fs.readFileSync(path.resolve(__dirname, '../src/index.html')).toString('utf8');
+        const { JSDOM } = jsdom;
+        const dom = new JSDOM(index);
+        const h1 = dom.window.document.getElementsByTagName("h1")[0];
+        expect(h1.innerHTML).to.equal("Users");
+        dom.window.close();
+    })
+
+    //TODO: Create API Response Test
+
+
+    //TODO: Create local JSON test
+
+
+    //TODO: Create returned data test
+
+
 })
---
+++ test/userAPI.test.js
@@ -0,0 +1,41 @@
+let expect = require('chai').expect,
+    supertest = require('supertest'),
+    express = require('express'),
+    api = supertest('http://localhost:3001');
+
+let app = express();
+
+if (!app.settings.env === 'production') {
+    describe('Users', function () {
+        // It should return 200
+        it('should return 200 response', function (done) {
+            api.get('/users')
+                .set('Accept', 'application/json')
+                .expect(200, done);
+        });
+
+        // It should return user data
+        it('should be a json object with keys and values', function (done) {
+            api.get('/users')
+                .set('Accept', 'application/json')
+                .expect(200)
+                .end(function (err, res) {
+                    expect(res).to.be.a('object');
+
+                    //expect(res.body).to.have.property("id");
+                    //expect(res.body.id).to.not.equal(null);
+
+                    //expect(res.body).to.have.property("firstName");
+                    //expect(res.body.firstName).to.not.equal(null);
+
+                    //expect(res.body).to.have.property("lastName");
+                    //expect(res.body.lastName).to.not.equal(null);
+
+                    //expect(res.body).to.have.property("email");
+                    //expect(res.body.email).to.not.equal(null);
+
+                    done();
+                })
+        })
+    })
+}
